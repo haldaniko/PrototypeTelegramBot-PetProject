973dd9b99f559412180fa24d9022d2281f0583ea
mirinum <edien.pierce.wd@gmail.com>
2022-02-03 17:36:34 +0200

    Added some stuff that needs testing, tomorrow weather DOESN'T work

---
+++ PrototypeWikipedia.py
@@ -0,0 +1,6 @@
+import wikipedia
+
+def prototype_wiki_search(request, language = "ru"):
+	wikipedia.set_lang(language)
+	suggestions = wikipedia.search(request, results = 5) # может пригодиться, но пока что это просто для того чтобы было
+	return wikipedia.summary(suggestions[0])
--- prototype.py
+++ prototype.py
@@ -1,20 +1,37 @@
 import telebot
 from WeatherForecast import *
 from ExchangeRates import *
+from PrototypeWikipedia import *
 
 bot = telebot.TeleBot("2105876367:AAHtzHrScKaxwzeo654CvvYXOtX9paiiq1Q")
 
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text.lower() == "привет":
-        bot.send_message(message.from_user.id, 'Привет, напиши "Погода"')
-    elif message.text.lower() == "погода":
-        bot.send_message(message.from_user.id, '`Погода на сегодня:\n' +
-                         request_forecast_today(get_city_id("Kiev")) + "`", parse_mode="Markdown")
-    elif message.text.lower() == "курс":
-        bot.send_message(message.from_user.id, '`Курс валют на сегодня:\n' +
-                         get_rate_info() + "`", parse_mode="Markdown")
-
+    try:
+        msg = message.text.lower() # чтобы на каждый elif не делать лишние трансформации строки, сразу запишем её
+        if msg == "привет":
+            bot.send_message(message.from_user.id, 'Привет, напиши "Погода"')
+        elif msg == "погода":
+            bot.send_message(message.from_user.id, '`Погода на сегодня:\n' +
+                             request_forecast_today(get_city_id("Kiev")) + "`", parse_mode="Markdown")
+        elif msg == "погода завтра":
+            bot.send_message(message.from_user.id, '`Погода на завтра:\n' +
+                             request_forecast_tomorrow(get_city_id("Kiev")) + "`", parse_mode="Markdown")
+        elif msg == "курс":
+            bot.send_message(message.from_user.id, '`Курс валют на сегодня:\n' +
+                             get_rate_info() + "`", parse_mode="Markdown")
+        elif msg.startswith(("что такое ", "расскажи про ", "кто такой ")):
+            request = msg.removeprefix('что такое ').removeprefix('расскажи про ').removeprefix('кто такой ')
+            bot.send_message(message.from_user.id, 'Ищу информацию про ' + request)
+            bot.send_message(message.from_user.id, 'Вот что я нашёл:\n' + prototype_wiki_search(request), parse_mode="Markdown")
+        elif msg.startswith(("what is ", "tell me about ", "who is ")):
+            request = msg.removeprefix('what is ').removeprefix('tell me about ').removeprefix('who is ')
+            bot.send_message(message.from_user.id, 'Looking for ' + request)
+            bot.send_message(message.from_user.id, "Look what I've found:\n" + prototype_wiki_search(request, "en"), parse_mode="Markdown")
+    except TypeError as e:
+        print("Wild Type Error occured! It uses \033[93m", e)
+        print('\033[0m')
+        pass
 
 bot.polling(none_stop=True, interval=0)
